5aad45f54ba5324ea480458317257c923fc1c07e
AzureAD\KavishMojhoa-Zapproa <kavish@zapproach.com>
2024-10-16 04:32:19 +0400

    bounce effect

--- src/app/components/loader.js
+++ src/app/components/loader.js
@@ -15,15 +15,15 @@ export default function Loader() {
         textAlign: "center"
       }}
     >
-      {/* Spinning Image */}
+      {/* Bouncing Image */}
       <motion.img
         src="/images/image.png" // Replace with your image path
         alt="Loading"
-        animate={{ rotate: 360 }}
+        animate={{ y: [0, -30, 0] }} // Bounce effect with keyframes
         transition={{
-          repeat: Infinity,
-          duration: 1,
-          ease: "linear"
+          repeat: Infinity, // Infinite bounce
+          duration: 0.6, // Speed of the bounce
+          ease: "easeInOut" // Smooth easing for bouncing
         }}
         style={{ width: "100px", height: "100px" }} // Adjust size as needed
       />
